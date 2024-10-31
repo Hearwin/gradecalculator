f649989bad3bdf9a94508ce9ea8fbf255a9efabe
janvichr001 <janvichr001@warren.k12.in.us>
2024-10-31 10:19:41 -0400

    calculator

---
+++ main.py
@@ -0,0 +1,16 @@
+int(input("80"))
+
+grade = "A"
+
+if  score >= 90:
+    grade = "A"
+elif score >= 80 and score < 90:
+    grade = "B"
+elif score >= 70 and score < 80:
+    grade = "C"
+elif score>= 60 and score < 70:
+    grade = "D"
+else: score <= 60
+    grade = "F"
+
+print("Your grade is ", grade)
